b95f0fb8a5d518b6a8b1f07fa299d36813eb33b0
Jonas Bulik <jonas@bulik.dev>
2020-07-11 01:39:21 +0200

    move and slide
    Signed-off-by: Jonas Bulik <jonas@bulik.dev>

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 export var SPEED = 128.0
 
-func _process(delta):
+
+func _physics_process(_delta):
 	var x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
 	var y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
-	self.move_and_collide(Vector2(x, y).normalized() * delta * SPEED)
+	print(self.move_and_slide(Vector2(x, y).normalized() * SPEED))
