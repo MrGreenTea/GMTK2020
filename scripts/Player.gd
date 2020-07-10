0880edbf1e7f2046e363556781a24e78439115a7
Jonas Bulik <jonas@bulik.dev>
2020-07-11 01:30:39 +0200

    add tileset and movement with collision
    Signed-off-by: Jonas Bulik <jonas@bulik.dev>

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
+export var SPEED = 128.0
+
 func _process(delta):
 	var x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
 	var y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
-	self.translate(Vector2(x, y).normalized())
+	self.move_and_collide(Vector2(x, y).normalized() * delta * SPEED)
