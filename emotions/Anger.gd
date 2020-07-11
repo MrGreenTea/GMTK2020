789a2dd8b69b19e3dc889a0b7959b346486df78b
Jonas Bulik <jonas@bulik.dev>
2020-07-11 23:09:47 +0200

    add street and house map
    Signed-off-by: Jonas Bulik <jonas@bulik.dev>

--- emotions/Anger.gd
+++ emotions/Anger.gd
@@ -1,21 +1,17 @@
 extends State
 
+
 func _on_Tween_tween_all_completed():
 	go_to("PlayerController")
 
 
 func on_enter(target: Player):
-	target.anger = 0
-	target.fear *= 0.1
 	target.get_node("AnimatedSprite").frame = 3
 	for i in range(10):
-		var offset = Vector2(rand_range(-1.0, 1.0), rand_range(-1.0, 1.0)).normalized() * 512
+		var offset = Vector2(rand_range(-1.0, 1.0), rand_range(-1.0, 1.0)).normalized() * 512		
 		$Tween.interpolate_method(target, "move_and_slide", Vector2.ZERO, offset, 0.3, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT, i * 0.3)
 	$Tween.start()
 
 func on_physics_process(target: Player, delta: float) -> void:
-	for o in target.get_node("DestructionArea").get_overlapping_bodies():
+	for o in target.get_node("Area2D").get_overlapping_bodies():
 		o.queue_free()
-	for p in target.get_node("PushArea").get_overlapping_bodies():
-		var dir = p.position - target.position
-		$Tween.interpolate_method(p, "move_and_slide", dir, dir * 2, 0.2, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
--- emotions/Fear.gd
+++ emotions/Fear.gd
@@ -5,14 +5,13 @@ export(float, 0.1, 10.0) var SPEED_MULTIPLIER = 1.5
 func on_enter(target: Player):
 	$Timer.start()
 	target.get_node("AnimatedSprite").frame = 4
-	target.fear = 0
 
 func on_physics_process(target: Player, delta: float):
 	var enemies = get_tree().get_nodes_in_group("enemies")
 	var direction = Vector2.ZERO
 	for e in enemies:
 		var distance = target.position - e.position
-		direction += distance / distance.length_squared()
+		direction += distance / distance.length()
 	target.move_and_slide(direction.normalized() * target.SPEED * SPEED_MULTIPLIER)
 
 
