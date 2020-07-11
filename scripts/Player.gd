d3fc700f191b2da4beedf67479ac5940dc75d715
Jonas Bulik <jonas@bulik.dev>
2020-07-11 10:36:47 +0200

    make tileset external resource
    Signed-off-by: Jonas Bulik <jonas@bulik.dev>

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,4 +6,4 @@ export var SPEED = 128.0
 func _physics_process(_delta):
 	var x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
 	var y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
-	print(self.move_and_slide(Vector2(x, y).normalized() * SPEED))
+	self.move_and_slide(Vector2(x, y).normalized() * SPEED)
