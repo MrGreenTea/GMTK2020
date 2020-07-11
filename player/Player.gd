a266ee9f772f79e36089875b5e290c55cf7db192
Jonas Bulik <jonas@bulik.dev>
2020-07-11 17:15:59 +0200

    add fear emotion with running away
    Signed-off-by: Jonas Bulik <jonas@bulik.dev>

--- player/Player.gd
+++ player/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 class_name Player
 
+export var SPEED = 128.0
 
 func _on_Target_body_entered(body):
 	print("YOU WIN")
--- player/PlayerController.gd
+++ player/PlayerController.gd
@@ -1,9 +1,9 @@
 extends State
 
 var stress = 0
-const MAX_STRESS = 10.0
-export var SPEED = 128.0
+export var MAX_STRESS = 10.0
 
+const EMOTIONS = ["Anger", "Fear"]
 
 func on_enter(target: Player):
 	target.get_node("AnimatedSprite").frame = 0
@@ -13,11 +13,11 @@ func on_physics_process(target: Player, delta: float) -> void:
 	var x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
 	var y = Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
 	var direction = Vector2(x, y).normalized()
-	var _a = target.move_and_slide(direction * SPEED)
+	var _a = target.move_and_slide(direction * target.SPEED)
 	stress += delta
 	var frame = round(float(stress) / MAX_STRESS * 2)
 	target.get_node("AnimatedSprite").frame = frame
 	if stress >= MAX_STRESS:
 		stress = 0
 		print("I am stressed as fuck!!")
-		go_to("Anger")
+		go_to(EMOTIONS[randi() % len(EMOTIONS)])
