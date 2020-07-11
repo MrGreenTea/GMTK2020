e1aae997a98de87df476cb520497017fe6f66dc4
Amon Lahr <i.amon@outlook.com>
2020-07-11 14:24:18 +0200

    Follow player to last seen position

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -5,26 +5,26 @@ export var SPEED = 128.0
 var follow_target = null
 var follow_target_last_position = null
 
+const follow_tolerance = 10
+
 func _ready():
 	# get player node
 	pass
 	
 func _physics_process(delta):
-	$Eyesight.scan(rotation)
+	$Eyesight.scan()
 	if follow_target != null:
-		var direction = (follow_target.position - position).normalized()
-		move_and_slide(SPEED * direction)
-
-#func look_for_target(target):
-#	$Eyesight.cast_to = target
-#	$Eyesight.fo
+		var run_to = follow_target - position
+		if run_to.length() > follow_tolerance:
+			var direction = (follow_target - position).normalized()
+			move_and_slide(SPEED * direction)
 
 func _on_SenseVisual_body_entered(body):
 	if body.name == "Player":
 		follow_target = body
 
 func _on_Eyesight_player_noticed(node):
-	follow_target = node
+	follow_target = node.position
 
 func _on_Eyesight_player_lost(node):
-	follow_target = null # Replace with function body.
+	print("Lost player!")
--- enemy/Eyesight.gd
+++ enemy/Eyesight.gd
@@ -5,18 +5,14 @@ export(float) var eyesight_angle_degrees = 360
 export(float) var eyesight_angle_steps = 180
 
 var visible_things = []
-var eyesight_angle_radians = PI
 var player
 
-signal entity_noticed(node)
 signal player_noticed(node)
 signal player_lost(node)
 
 func _ready():
-	eyesight_angle_radians = deg2rad(eyesight_angle_degrees)
 	# find player
-	for node in get_tree().get_nodes_in_group("visible_people"):
-		if node.name == "Player":
+	for node in get_tree().get_nodes_in_group("player"):
 			player = node
 			prints("Found player", player)
 	
@@ -25,20 +21,11 @@ func _ready():
 		add_exception(node)
 		prints("Added exception", node)
 	
-func scan(scan_angle):
-	visible_things = []
-	#for angle in range(scan_angle-eyesight_angle_radians/2, scan_angle+eyesight_angle_radians/2, eyesight_angle_steps):
-	#var player_direction = 
-	look_at(player.position)  #player.position# - position
-	#set_cast_to(player.position)
-	#set_cast_to(Vector2(0,1).rotated(rotation))
-	# force_raycast_update()
+func scan():
+	look_at(player.position) # could be replaced with cast_to = ...
 	var object = get_collider()
-	#prints(object)
-	# prints(Vector2(0,1).rotated(angle) * eyesight_radius, object)
 	if object != null && object.name == "Player":
-		#visible_things.append(object)
 		emit_signal("player_noticed", player)
 	else:
 		emit_signal("player_lost", player)
-	#prints(scan_angle, visible_things)
+
