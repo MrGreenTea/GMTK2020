bd7863b38cc02533c66b1d043cf42c564f31c35e
Amon Lahr <i.amon@outlook.com>
2020-07-11 12:24:22 +0200

    Add eyesight scan, still under construction

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -3,16 +3,21 @@ extends KinematicBody2D
 export var SPEED = 128.0
 
 var follow_target = null
+var follow_target_last_position = null
 
 func _ready():
 	# get player node
 	pass
 	
-func _process(delta):
+func _physics_process(delta):
+	$Eyesight.scan(rotation)
 	if follow_target != null:
 		var direction = (follow_target.position - position).normalized()
 		move_and_slide(SPEED * direction)
 
+#func look_for_target(target):
+#	$Eyesight.cast_to = target
+#	$Eyesight.fo
 
 func _on_SenseVisual_body_entered(body):
 	follow_target = body
