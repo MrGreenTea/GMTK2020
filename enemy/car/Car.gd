fa19e8be728093a9b8d2f652ca84011703da7ea2
Jonas Bulik <jonas@bulik.dev>
2020-07-12 18:37:13 +0200

    add car crash with sounds
    Signed-off-by: Jonas Bulik <jonas@bulik.dev>

--- enemy/car/Car.gd
+++ enemy/car/Car.gd
@@ -10,6 +10,7 @@ func _ready():
 	var variation = randi() % VARIATIONS + 1
 	var image = load("res://enemy/car/car_%s_%d.png" % [color, variation])
 	$Sprite.texture = image
+	$AudioStreamPlayer2D.seek(rand_range(0, 1.0))
 	
 
 func _physics_process(delta):
@@ -18,4 +19,7 @@ func _physics_process(delta):
 
 func _on_Area2D_body_entered(body):
 	if body is Player:
+		$AudioStreamPlayer2D.stream = preload("res://sound/tires.ogg")
+		$AudioStreamPlayer2D.play()
+		$AudioStreamPlayer2D.volume_db = 0
 		body.show_game_over("car_crash")
