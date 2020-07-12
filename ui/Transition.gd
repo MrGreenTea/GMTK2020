839307bcc203fa64fc8593eb1b19be73e82d1c4c
Amon Lahr <i.amon@outlook.com>
2020-07-12 14:45:43 +0200

    fix transition not showing

--- ui/Transition.gd
+++ ui/Transition.gd
@@ -7,8 +7,11 @@ func _ready():
 	player.stop()
 
 func transition_in():
+	visible = true
 	player.play("transition_out")
 
 func transition_out():
+	print("PLAX")
+	visible = true
 	player.play_backwards("transition_out")
 
